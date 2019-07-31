dbd4c564bee0bf781b064c815d78da53d332bbcd
Paul Boocock <mail@paulboocock.net>
2019-07-31 22:24:13 +0100

    Update about page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,8 +12,10 @@ export default () => (
             <div className={aboutStyles.description}>
                 <h1>About</h1>
                 <h2>A little more about me</h2>
-                <p>I'm a Software Engineer who has a keen interest in process improvement and working closely with those around me. I'm a advocate of Lean processes and my thinking is heavily influenced by the Toyota Production System.</p>
-                <p>Throughout my career I have worked with a variety of different programming languages and always enjoy learning new technologies. I'm an advocate for Clean Coding techniques and reducing technical debt whenever possible. I have a good understanding of fundamental software engineering techniques and principles and I throughly enjoying sharing these thoughts with others.</p>            
+                <p>I'm a Software Engineer with a fascination for Data. I'm particularly interested in Data Analytics and I'm excited by the possibilities that data opens up to the world, although I'm also very aware of the social responsibility we all share when it comes to Data.</p>
+                <p>I'm a advocate of Lean processes and my thinking is heavily influenced by the Toyota Production System. There is nothing I appreciate more than a lean process that allows me to get things done, to deliver quickly and to a high quality.</p>
+                <p>Throughout my career I have worked with a variety of different programming languages and I see myself as a diverse multi-disciplined software engineer. I always enjoy learning new technologies and never shy away from a new opportunity to do so.</p>
+                <p>I'm an advocate for Clean Coding techniques and reducing technical debt whenever possible. I have a solid (pun intended) understanding of fundamental software engineering techniques and principles and I throughly enjoying sharing these thoughts with others.</p>            
             </div>
         </div>
     </Layout>
